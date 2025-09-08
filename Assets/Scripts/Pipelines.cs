c51feeefdf53f4345dcaf1d82b493b6e1b46c1e3
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-09-08 12:36:42 -0400

    Part B task 3 done

--- Assets/Scripts/Pipelines.cs
+++ Assets/Scripts/Pipelines.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 public class Pipelines : MonoBehaviour
 {
     public Vector3 storeMousePosition;
+    public Vector3 storeMousePositionEnd;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Debug.DrawLine(storeMousePosition, storeMousePositionEnd);
+        if (Input.GetMouseButtonUp(0))
         {
-            storeMousePosition = Input.mousePosition;
+            storeMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log("Mouse Position:" + storeMousePosition);
+
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            storeMousePositionEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("Mouse Position:" + storeMousePositionEnd);
         }
     }
 }
