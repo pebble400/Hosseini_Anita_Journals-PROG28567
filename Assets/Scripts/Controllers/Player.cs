f00b86d48b0448fc68b84f3adcd34c8796921c66
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2025-09-09 12:46:27 -0400

    imported a scene

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
