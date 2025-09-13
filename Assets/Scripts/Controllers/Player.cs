c004a4c0f4e2804c2833a99cf6455440b3daa054
anitahosseini37@gmail.com <anitahosseini37@gmail.com>
2025-09-12 22:12:55 -0400

    Task 1 completed

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using JetBrains.Annotations;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public Transform enemyTransform;
     public GameObject bombPrefab;
+    public int howManyBombs = 10;
     public List<Transform> asteroidTransforms;
 
     // Update is called once per frame
@@ -15,11 +17,26 @@ public class Player : MonoBehaviour
         {
             SpawnBombAtOffset(new Vector3(0, 1));
         }
+        if (Input.GetKeyDown(KeyCode.T)) 
+        {
+            numberOfBombs(1, howManyBombs, new Vector3(0, 1));
+        }
     }
 
      public void SpawnBombAtOffset(Vector3 inOffset)
     {
-        Vector3 spawnPosition = transform.position + inOffset;
-        Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        GameObject newBomb = Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+
+
+    }
+    public void numberOfBombs(float bombSpacing, int innumberOfBombs, Vector3 inOffset)
+    {
+        for(int i = 0; i < innumberOfBombs; i++)
+        {
+            GameObject newBomb = Instantiate(bombPrefab);
+            Vector3 spawnPosition = transform.position - inOffset - new Vector3(0, bombSpacing * i);
+            newBomb.transform.position = spawnPosition;
+        }
+       
     }
 }
